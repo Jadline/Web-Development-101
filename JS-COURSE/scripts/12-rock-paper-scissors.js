1316ef93d5a963a8ebfa2d738be4c2df08a4db4c
Jadline <njerijadline@gmail.com>
2024-07-06 13:07:10 +0300

    update changes

--- JS-COURSE/scripts/12-rock-paper-scissors.js
+++ JS-COURSE/scripts/12-rock-paper-scissors.js
@@ -38,6 +38,18 @@ let score = JSON.parse(localStorage.getItem('score'))
         document.querySelector('.js-scissors-button').addEventListener('click',() => {
             playGame('scissors')
         })
+        document.body.addEventListener('keydown',(event) => {
+            if(event.key === 'r') {
+                playGame('rock')
+            }
+            else if (event.key === 'p') {
+                playGame('paper')
+            }
+            else if (event.key === 's') {
+                playGame('scissors')
+            }
+            
+        }) 
     
       function playGame(playerMove){
         const computerMove = pickComputerMove();
--- JS-COURSE/scripts/12-todo-list.js
+++ JS-COURSE/scripts/12-todo-list.js
@@ -31,10 +31,7 @@ function renderTodoList(){
         
         const html = `<div>${name}</div>
         <div>${dueDate}</div>
-        <button onclick="
-            todoList.splice(${index},1)
-            renderTodoList()
-        " class="delete-todo-button">Delete</button>
+        <button class="delete-todo-button js-delete-todo-button">Delete</button>
         `
         todoListHTML += html 
     })
@@ -53,10 +50,18 @@ function renderTodoList(){
         todoListHTML += html
 }*/
     document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+    document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton,index) => {
+        deleteButton.addEventListener('click',()=>{
+            todoList.splice(index,1)
+            renderTodoList()
+        })
+    })
 
 }
 
-
+document.querySelector('.js-add-todo-button').addEventListener('click',() => {
+    addTodo()
+})
 function addTodo(){
     const inputElement = document.querySelector('.js-name-input')
     const name = inputElement.value
